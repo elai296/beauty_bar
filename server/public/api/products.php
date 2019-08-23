8b1435a259876995ba952e9b5c635270cea2ca25
Elaine Phan <elainep926@gmail.com>
2019-08-23 00:52:46 +0000

    Create connection file for mysql-Back end

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,11 +1,7 @@
 <?php
+header('Content-Type: application/json');
 require_once('functions.php');
-// header('Content-Type: application/json');
-
-// if (empty($_GET['id'])) {
-//   readfile('dummy-products-list.json');
-// } else {
-//   readfile('dummy-product-details.json');
+require_once('db_connection.php');
 
 set_exception_handler('error_handler');
 
