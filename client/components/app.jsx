0830b93c70ba930b91016eea7624c1813f2aa5da
Elaine Phan <45439217+elai296@users.noreply.github.com>
2019-12-16 18:57:51 -0800

    Revert "edited getCartItem"

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -40,7 +40,8 @@ export default class App extends React.Component {
         response.json()
       )
       .then(cartProducts => {
-        this.setState({ cart: cartProducts }, this.setState({ cartItems: this.sumCartItem(this.state.cart) }));
+        this.setState({ cart: cartProducts });
+        this.setState({ cartItems: this.sumCartItem(this.state.cart) });
       });
   }
 
